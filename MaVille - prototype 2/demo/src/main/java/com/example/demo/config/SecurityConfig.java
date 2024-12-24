0215adc34e5787b5e367582d24aa73e3a5c364a5
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2024-12-23 20:07:48 -0500

    final edit backend cors

--- MaVille - prototype 2/demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ MaVille - prototype 2/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
             "http://localhost:5173",  // Dev frontend
-            "https://maville-production.up.railway.app"  // Production frontend
+            "https://maville-production.up.railway.app", // Production frontend
+            "https://maville-frontend-production.up.railway.app"  // Production frontend avec frontend title
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
